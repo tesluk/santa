a3b90404aef9a5d4c18c8f86d33820f461d35054
atesluk <vdjtab@gmail.com>
2014-11-28 18:52:26 +0200

    Deploy

---
+++ src/main/java/tab/os/servlet/SantaServlet.java
@@ -0,0 +1,40 @@
+package tab.os.servlet;
+
+import tab.os.tools.SantaCipher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by andrey.tesluk on 28.11.2014.
+ */
+public class SantaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/whoismine.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String encryptedText = req.getParameter("text");
+
+            String myPrivateKey = req.getParameter("key");
+
+            String[] encryptedRows = encryptedText.split("\n");
+
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < encryptedRows.length; i++) {
+                String decrypted = SantaCipher.decrypt(encryptedRows[i], myPrivateKey);
+                res.append(String.format("%d) %s  \n", i, decrypted));
+            }
+            resp.getWriter().println(res.toString());
+        } catch (Exception e) {
+            resp.getWriter().println("Google 241543903");
+        }
+    }
+}
---
+++ src/main/webapp/whoismine.jsp
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Welcome to OpenShift</title>
+</head>
+
+<body>
+<form method="post" action="/whoismine">
+    <textarea name="text" style="width: 300px; word-break: break-all"></textarea>
+    <br>
+    <input type="text" name="key" style="width: 300px" placeholder="your private key..."/>
+    <br>
+    <input type="submit">
+</form>
+</body>
+</html>
